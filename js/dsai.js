31517f10ef54cb6a9a08de3815dbe4ac36afbeff
Rudder <ben@ruddernation.com>
2022-05-25 21:47:13 +0100

    Wrong Folder

--- js/dsai.js
+++
@@ -1,5 +0,0 @@
-jQuery('.ds').click(function(e){
-  e.preventDefault();  
-  var val = jQuery('#replycontent').val(); 
-  jQuery('#replycontent').val(val + "\r\n" + jQuery(this).data('text')); 
-});
